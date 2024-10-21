050166318ff5a89859c63669638f91232bdde105
Tomas Laprovitta <tlaprovitta@gmail.com>
2024-10-21 20:56:40 -0300

    Estructura + Renderizado con Base de Datos Externa

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,36 @@
-console.log(tempData);
+const section = document.getElementById('movies');
+
+$.get("https://students-api.up.railway.app/movies", (data) => {
+    renderCards(data);
+});
+
+const renderCards = (data) => {
+
+    data.forEach(movie => {
+        const newDivCard = document.createElement('div');
+        newDivCard.classList.add('card-movie');
+        
+    const newLink = document.createElement('a');
+    newLink.href = "#"
+
+    const newDivContainer = document.createElement('div');
+    newDivContainer.classList.add('image-container')
+    const newImg = document.createElement('img');
+    newImg.src = movie.poster;
+    newImg.classList.add('image-movie')
+    const newTitle = document.createElement('h2');
+    newTitle.classList.add('title')
+    newTitle.innerHTML = movie.title;
+    const newYear = document.createElement('h2');
+    newYear.classList.add('year');
+    newYear.innerHTML = movie.year;
+    
+    newDivContainer.append(newImg, newTitle, newYear);
+    newLink.append(newDivContainer);
+    newDivCard.append(newLink);
+    
+
+    section.append(newDivCard);
+    
+});
+};
